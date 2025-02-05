e701acde3b8010cf2afbded06e54ad6c6a69a123
Myo Thiha <mth2682004@gmail.com>
2025-02-05 23:28:49 +0000

    Finished the background gradient of album section

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
-import { albumsData, assets } from "../assets/frontend-assets/assets";
+import {
+  albumsData,
+  assets,
+  songsData,
+} from "../assets/frontend-assets/assets";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
@@ -36,6 +40,22 @@ const DisplayAlbum = () => {
         <p className="hidden sm:block">Date Added</p>
         <img className="m-auto w-4" src={assets.clock_icon} alt="" />
       </div>
+      <hr />
+      {songsData.map((song, index) => (
+        <div
+          key={index}
+          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white">
+            <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img className="inline w-10 mr-5" src={song.image} alt="" />
+            {song.name}
+          </p>
+          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block">{albumData.name}</p>
+          <p className="text-[15px] text-center">{song.duration}</p>
+        </div>
+      ))}
     </>
   );
 };
